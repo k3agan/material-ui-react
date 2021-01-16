c9df31fbf285352002f9557601d7683b9c6a973c
K3agan <k3agan@gmail.com>
2021-01-16 13:09:03 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import {useState, useEffect} from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles  = makeStyles({
+  root: {
+    border: 0,
+    borderRadius: 15,
+    background: 'linear-gradient(45def,#333, #999)',
+    color: 'secondary'
+    
+  }
+})
+
+const CheckboxExample = () => {
+  const [checked,setChecked ] = useState(true)
+  return(
+    <FormControlLabel 
+    control = {<Checkbox
+      checked = {checked}
+      icon = {<DeleteIcon color = 'secondary' />}
+      checkedIcon = {<SaveIcon color = 'primary'/>}
+      onChange = {(e)=>setChecked (e.target.checked)}
+      inputProps = {{
+        'aria-label': 'secondary checkbox'
+      }}
+    />}
+    label = 'testing'
+    />
+      
+      
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <TextField
+        variant = 'filled'
+        color = 'secondary'
+        type = 'email'
+        label = 'The time'
+        placeholder = "test@test.ca"
+        />
+        <CheckboxExample/>
+        <ButtonGroup 
+        color = "primary"
+        variant = "contained"
+        size = 'large' >
+        <Button 
+        startIcon = {<SaveIcon/>}
         >
-          Learn React
-        </a>
+          Save
+        </Button>
+        <Button 
+        startIcon = {<DeleteIcon/>}
+        color = "secondary">
+          Discard
+        </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
